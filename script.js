d1bb0e04dcb112d41eb1ea7b4df7666237c423e9
Michael Ayozie <michaelayozie5@gmai.com>
2022-10-04 17:43:40 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+
+window.addEventListener("load", () => {
+  const loader = document.querySelector(".loader");
+
+  loader.classList.add("loader--hidden");
+
+  loader.addEventListener("transitionend", () => {
+    document.body.removeChild(loader);
+  });
+});
+
+
+window.onload = () => {
+  setTimeout(() => {
+    document.querySelector("body").classList.add("display");
+  }, 4000);
+};
+
+document.querySelector(".menu").addEventListener("click", () => {
+  document.querySelector(".container").classList.toggle("change");
+});
+
+document.querySelector(".scroll-btn").addEventListener("click", () => {
+  document.querySelector("html").style.scrollBehavior = "smooth";
+  setTimeout(() => {
+    document.querySelector("html").style.scrollBehavior = "unset";
+  }, 1000);
+});
+
